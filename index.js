8bbca2ea91f5a1729d209c9b8037373bba3bca61
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-08 03:40:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,39 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for (let idx = 0; idx < newCollection.length; idx++)
+        iteratee(newCollection[idx])
+
+      return collection
+      },
+
+    map: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      const newC = [];
+      for (let i = 0; i < newCollection.length; i++) {
+          newC.push(iteratee(newCollection[i]))
+
+
+    }
+    return newC
     },
+
+    reduce: function(collection, iteratee, acc) {
+         collection = (collection instanceof Array) ? collection.slice() : Object.values(collection);
+         if (!acc) {
+          acc = collection[0];
+          collection = collection.slice(1);
+         }
+         for (let i = 0; i < collection.length; i++) {
+
+              acc = iteratee(acc, collection[i]);
+
+          }
+            return acc;
+,
 
     map: function() {
 
