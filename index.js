4f5e37367a898c7e6664772923cf4c212e9c7dff
Robert Clarke <r.jason.clarke@gmail.com>
2021-03-08 03:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,173 +4,115 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(collection, iteratee) {
-      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+    // calls alert with each element passed
+// calls alert properly on object values
+// returns the original collection
+each: function(collection, callback) {
 
-      for (let idx = 0; idx < newCollection.length; idx++)
-        iteratee(newCollection[idx])
+    let dub = (collection instanceof Array) ? collection.split() : Object.values(collection);
 
-      return collection
-      },
+    // if (collection instanceof Array) {
+    //    collection.split();
+    // } else {
+    //    Object.values(collection);
+    // }
 
-    map: function(collection, iteratee) {
-      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
-      const newC = [];
-      for (let i = 0; i < newCollection.length; i++) {
-          newC.push(iteratee(newCollection[i]))
-
-
+    for (let i=0; i < dub.length; i++) {
+        callback(dub[i]);
     }
-    return newC
-    },
 
-    reduce: function(collection, iteratee, acc) {
-         collection = (collection instanceof Array) ? collection.slice() : Object.values(collection);
-         if (!acc) {
-          acc = collection[0];
-          collection = collection.slice(1);
-         }
-         for (let i = 0; i < collection.length; i++) {
-
-              acc = iteratee(acc, collection[i]);
-
-          }
-            return acc;
-
-
-find: function(arr, finderFn) {
-  let newArr = (arr instanceof Array) ? arr.slice() : Object.values(arr);
-  for (let i = 0; i < newArr.length; i++) {
-    if (finderFn(newArr[i])) {
-      return newArr[i]
-    }
-  }
+    return collection;
 },
 
-filter: function(arr, excluder) {
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (excluder(arr[i])) {
-      newArr.push(arr[i]);
+// successfully returns a correctly populated array
+// does not modify the original array
+// successfully returns a correctly populated array from modified object values
+// does not modify the original object
+map: function(collection, callback) {
+
+    if (!(collection instanceof Array)) {
+        collection = Object.values(collection)
     }
-  }
-  return newArr;
+
+    const newCollection = []
+
+    for (let i = 0; i < collection.length; i++){
+        newCollection.push(callback(collection[i]))
+    }
+
+    return newCollection;
 },
 
-size: function(thing) {
-  if (thing instanceof Array) {
-    return thing.length;
-  }
-  return Object.keys(thing).length;
+// returns the correct reduced value when passed an accumulator
+// returns the correct reduced value when not passed an accumulator
+// does not modify the original array
+reduce: function(c=[], callback = () => {}, accumulator) {
+    let collection = c.slice(0)
 
-    },
+if (!accumulator) {
+  accumulator = collection[0]
+  collection = collection.slice(1)
+}
 
-    first: function(arr, arg) {
-         let newArr = [];
-         if (arg) {
-           for (let i = 0; i < arg; i++) {
-             newArr.push(arr[i]);
-           }
-         return newArr;
-         }
-         else {
-           return arr[0];
-         }
-       },
+for (let i=0; i < collection.length; i++) {
+  accumulator = callback(accumulator, collection[i], collection)
+}
 
-       last: function(arr, arg) {
-         let newArr = [];
-         if (arg) {
-           for (let i = 0; i < arg; i++) {
-             newArr.unshift(arr[arr.length - 1 - i]);
-           }
-         return newArr;
-         }
-         else {
-           return arr[arr.length - 1];
-         }
-       },
+    return accumulator;
+},
 
-       compact: function(arr) {
-         let newArr = [];
-         for (let i = 0; i < arr.length; i++) {
-           if (arr[i]) {
-             newArr.push(arr[i]);
-           }
-         }
-         return newArr;
-       },
-       sortBy: function(collection, callback) {
-           const newArr = [...collection];
-           return newArr.sort(function (a, b) {
-             return callback(a) - callback(b)
-           })
-          },
+// returns the value if found
+// does not traverse the whole array if the value is found early
+// returns undefined if the value is not present
+find: function(collection, callback) {
+  if (!(collection instanceof Array))
+    collection = Object.values(collection)
 
-          unpack: function(receiver, arr) {
-             for (let val of arr)
-               receiver.push(val)
+  for (let i=0; i < collection.length; i++)
+    if (callback(collection[i])) return collection[i]
 
+  return undefined
+},
 
-    },
+// filters for values that the callback evaluates as true
+filter: function(collection, callback) {
+    if (!(collection instanceof Array)) {
+        collection = Object.values(collection)
+    }
 
-    flatten: function(collection, shallow, newArr=[]) {
-      if (!Array.isArray(collection)) return newArr.push(collection)
-      if (shallow) {
-        for (let val of collection)
-          Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
-      } else {
-        for (let val of collection) {
-          this.flatten(val, false, newArr)
-        }
-      }
-      return newArr
-    },
-    uniq: function(collection, sorted=false, iteratee=false) {
-      if (sorted) {
-        return fi.uniqSorted(collection, iteratee)
-      } else if (!iteratee) {
-        return Array.from(new Set(collection))
-      } else {
-        const modifiedVals = new Set()
-        const uniqVals = new Set()
-        for (let val of collection) {
-          const moddedVal = iteratee(val)
-          if (!modifiedVals.has(moddedVal)) {
-            modifiedVals.add(moddedVal)
-            uniqVals.add(val)
-          }
-        }
-        return Array.from(uniqVals)
-      }
+    const newCollection = []
 
+    for (let i=0; i < collection.length; i++)
+        if (callback(collection[i])) newCollection.push(collection[i])
+
+    return newCollection
+},
+
+// returns the size of the collection when an array is passed
+// returns the size of the collection (amount of keys) when an object is passed
+size: function(collection) {
+    return (collection instanceof Array) ? collection.length : Object.keys(collection).length
+},
+
+// returns the first element of the collection
+// returns the first n elements of the collection when the second optional argument (n) is provided
+first: function(collection, stop=false) {
+    return (stop) ? collection.slice(0, stop) : collection[0]
 
 
     },
-    keys: function(test) {
-      let arr = []
-      for (let key in test) {
-        arr.push(key)
-      }
-      return arr;
+
+    map: function() {
+
     },
 
-    values: function(test) {
-      let arr = []
-      for (let value in test) {
-        arr.push(test[value])
-      }
-      return arr;
-    },
-    functions: function (obj) {
-          let arr = [];
-          for (let el in obj) {
-            if (typeof obj[el] === "function")
-              arr.push(el)
-          }
-          return arr
-        }
+    reduce: function() {
 
+    },
+
+    functions: function() {
+
+    },
 
 
   }
